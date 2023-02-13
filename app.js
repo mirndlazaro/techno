cb638ed78671647362177de5ce0f2c38d2601d84
Lazaro <lazaro.miranda@opensystem.srv.br>
2023-02-13 16:06:02 -0300

    Gerenciamento do modal

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const vm = new Vue({
     el: "#app",
     data: {
         produtos: [],
+        produto: false
     },
     filters: {
         numberToPrice(valor) {
@@ -14,7 +15,28 @@ const vm = new Vue({
                 .then(r => r.json())
                 .then(r => {
                     this.produtos = r;
+                }
+            )
+        },
+        getProdutoById: function(id) {
+            fetch(`./api/produtos/${id}/dados.json`)
+                .then(r => r.json())
+                .then(r => {
+                    this.produto = r;
                 })
+                .catch(error => {
+                    throw(error);
+                })
+        },
+        abrirModal(id) {
+            this.getProdutoById(id);
+            window.scrollTo({
+                top: 0,
+                behavior: "smooth"
+            })
+        },
+        fecharModal({ target, currentTarget }) {
+            if (currentTarget === target) this.produto = false;
         }
     },
     created() {
